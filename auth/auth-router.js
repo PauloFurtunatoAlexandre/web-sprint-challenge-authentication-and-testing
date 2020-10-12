298268ef876a42cbe98d9f49d86bc7c991ef04fc
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-10-11 21:22:17 -0500

    added jsonwebtoken correctly

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
+const jwt = require("jsonwebtoken");
 const { jwtSecret } = require('../config/secret.js');
 
 router.post("/register", (req, res) => {
