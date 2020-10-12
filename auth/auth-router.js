8025ae3f8c1e71656639a80c8524631910879c0d
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-10-11 20:40:43 -0500

    added the user registration

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,59 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model.js');
+const { jwtSecret } = require('../config/secret.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+	// implement registration
+	let user = req.body;
+
+	const hash = bcrypt.hash(user.password, 10);
+
+	user.password = hash;
+
+	Users.add(user)
+		.then((newUser) => {
+			res.status(201).json(newUser);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+	// implement login
+	let { username, password } = req.body;
+
+	Users.findBy({ username })
+		.first()
+		.then((user) => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = generateToken(user);
+				res.status(200).json({
+					message: `Welcome ${user.username}!`,
+					token: token,
+				});
+			} else {
+				res.status(401).json({ message: 'You shall not pass!' });
+			}
+		});
 });
 
+function generateToken(user) {
+	const payload = {
+		subject: user.id,
+		username: user.username,
+		role: user.role,
+	};
+
+	const secret = jwtSecret || 'another secret here';
+
+	const options = {
+		expiresIn: '1d',
+	};
+
+	const token = jwt.sign(payload, secret, options);
+
+	return token;
+}
 module.exports = router;
---
+++ users/users-model.js
@@ -0,0 +1,25 @@
+const db = require('../database/dbConfig.js');
+
+module.exports = {
+	add,
+	find,
+	findBy,
+	findById,
+};
+
+function find() {
+	return 'users'.select('id', 'username', 'password');
+}
+
+function findBy(filter) {
+	return 'users'.where(filter);
+}
+
+async function add(user) {
+	const [id] = await db('users').insert(user);
+	return findById('users');
+}
+
+function findById(id) {
+	return db('users').where({ id }).first();
+}
